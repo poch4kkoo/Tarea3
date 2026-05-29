df9f1275d474e8b81189a67aad3c788130725053
Javiera D <jd2025@udec.cl>
2026-05-29 14:15:10 -0400

    Clase PanelPrincipal

---
+++ src/main/java/org/grafica/PanelPrincipal.java
@@ -0,0 +1,24 @@
+package org.grafica;
+
+import org.logica.*;
+import javax.swing.*;
+import java.awt.*;
+
+public class PanelPrincipal extends JPanel {
+
+    private PanelComprador com;
+    private PanelExpendedor exp;
+
+    public PanelPrincipal() {
+        exp = new PanelExpendedor();
+        com = new PanelComprador();
+        this.setBackground(Color.white);
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        com.paintComponent(g);
+        exp.paintComponent(g);
+    }
+
+}
