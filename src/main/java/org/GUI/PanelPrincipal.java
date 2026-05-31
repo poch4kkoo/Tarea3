f7b4328185f0f7ed2af8417fb13367414bd7e446
Javiera D <jd2025@udec.cl>
2026-05-31 18:41:21 -0400

    Creacion panelcomprador en panel principal

--- src/main/java/org/GUI/PanelPrincipal.java
+++ src/main/java/org/GUI/PanelPrincipal.java
@@ -1,5 +1,6 @@
 package org.GUI;
 
+import org.logica.Comprador;
 import org.logica.Expendedor;
 
 import javax.swing.*;
@@ -7,7 +8,7 @@ import java.awt.*;
 
 public class PanelPrincipal extends JPanel {
 
-    //private PanelComprador com;
+    private PanelComprador com;
     private PanelExpendedor exp;
     private Expendedor llenarExp;
 
@@ -18,10 +19,11 @@ public class PanelPrincipal extends JPanel {
 
         llenarExp = new Expendedor(2);
         exp = new PanelExpendedor(llenarExp);
-        //     com = new PanelComprador();
+        com = new PanelComprador(new Comprador(), llenarExp);
         this.setBackground(Color.white);
 
         // Agregamos el componente al panel de forma real
         this.add(exp);
+        this.add(com);
     }
 }
