5e5bb61881201b594ef910670b3f6f5a21ae78c4
Javiera D <jd2025@udec.cl>
2026-06-09 22:06:46 -0400

    Se agrego un mensaje inicial con el precio e instrucciones del programa

--- src/main/java/org/GUI/Ventana.java
+++ src/main/java/org/GUI/Ventana.java
@@ -25,6 +25,23 @@ public class Ventana extends JFrame {
         this.setDefaultCloseOperation(EXIT_ON_CLOSE); //cerrar aplicación
         this.add(pri,BorderLayout.CENTER); //se agrega al centro
 
+        String mensajePrecios = "LISTA DE PRECIOS:\n"
+                + "--------------------------------------------------------\n"
+                + "Coca-Cola:    $1000\n"
+                + "Fanta:            $1000\n"
+                + "Sprite:           $1000\n"
+                + "Super 8:        $500\n"
+                + "Snickers:      $800\n"
+                + "--------------------------------------------------------\n"
+                + "INSTRUCCIONES:\n"
+                + "1. Haz clic en una moneda de tu monedero (Panel Derecho).\n"
+                + "2. Haz clic en la ranura gris de la máquina para ingresarla.\n"
+                + "3. Presiona el botón del producto que deseas comprar.\n"
+                + "4. Retire el producto del deposito, haciendo clic en el.\n"
+                + "5. Presione el boton consumir para comerse el producto.\n";
+
+        JOptionPane.showMessageDialog(this, mensajePrecios, "Menú de Precios e Instrucciones", JOptionPane.INFORMATION_MESSAGE);
+
         pack();
         this.setSize(VGUI.LARGO,VGUI.ALTO);
         setLocationRelativeTo(null);
